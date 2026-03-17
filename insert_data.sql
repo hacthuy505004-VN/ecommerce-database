6fa42fa583601a3349a1ffaf24c430160cc50e6d
Trịnh Đình Văn <hacthuy505004@gmail.com>
2026-03-17 18:28:20 +0700

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users (name, email) VALUES
+('Van', 'van@gmail.com'),
+('Nam', 'nam@gmail.com');
+
+INSERT INTO Products (name, price, stock) VALUES
+('Laptop', 1500, 10),
+('Phone', 800, 20),
+('Headphone', 200, 30);
